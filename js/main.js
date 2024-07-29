13a4e75161959a1c008905d794a736593e47268d
facundogoitia <facundo.goitia@gmail.com>
2024-07-29 15:00:52 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+
+var toastTrigger = document.getElementById('liveToastBtn1')
+var toastLiveExample = document.getElementById('liveToast1')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+
+var toastTrigger = document.getElementById('liveToastBtn2')
+var toastLiveExample = document.getElementById('liveToast2')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+
+var toastTrigger = document.getElementById('liveToastBtn3')
+var toastLiveExample = document.getElementById('liveToast3')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+
+var toastTrigger = document.getElementById('liveToastBtn4')
+var toastLiveExample = document.getElementById('liveToast4')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+
+var toastTrigger = document.getElementById('liveToastBtn5')
+var toastLiveExample = document.getElementById('liveToast5')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+
+var toastTrigger = document.getElementById('liveToastBtn6')
+var toastLiveExample = document.getElementById('liveToast6')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+
+var toastTrigger = document.getElementById('liveToastBtn7')
+var toastLiveExample = document.getElementById('liveToast7')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
